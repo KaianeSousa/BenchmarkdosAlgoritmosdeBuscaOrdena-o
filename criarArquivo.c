7102c05ab86b1a6f5105552755a57f2e92ef70a7
KaianeSousa <132933423+KaianeSousa@users.noreply.github.com>
2023-11-06 20:20:49 -0300

    Update criarArquivo.c
    com e sem  a main

--- criarArquivo.c
+++ criarArquivo.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+//Com Main
 int main(){
     
     int quantidade, numero;
@@ -49,3 +50,54 @@ int main(){
 
     return 0;
 }
+
+//Sem main
+
+#include <stdio.h>
+
+void adicionarNumerosArquivo(const char *nomeArquivo) {
+    int quantidade, numero;
+    FILE *arquivo = NULL; // Inicialize o ponteiro do arquivo como NULL
+
+    printf("Quantos números inteiros você deseja adicionar ao arquivo? ");
+    scanf("%d", &quantidade);
+
+    printf("Digite os números (um por linha):\n");
+
+    for (int i = 0; i < quantidade; i++) {
+        scanf("%d", &numero);
+
+        arquivo = fopen(nomeArquivo, "r");
+        int existe = 0;
+        int numLido;
+
+        if (arquivo != NULL) {
+            while (fscanf(arquivo, "%d", &numLido) != EOF) {
+                if (numLido == numero) {
+                    printf("O número %d já existe. Digite outro número: ", numero);
+                    i--; // Decrementa o contador para digitar um novo número
+                    fclose(arquivo); // Fecha o arquivo apenas se ele foi aberto
+                    arquivo = NULL; // Define o ponteiro do arquivo como NULL
+                    break;
+                }
+            }
+        }
+
+        if (existe != numero) {
+            arquivo = fopen(nomeArquivo, "a");
+            if (arquivo == NULL) {
+                printf("Erro ao abrir o arquivo.\n");
+                return;
+            }
+            fprintf(arquivo, "%d\n", numero); // Escreve o número no arquivo
+        }
+
+        if (arquivo != NULL) {
+            fclose(arquivo); // Fecha o arquivo se ele estiver aberto
+            arquivo = NULL; // Define o ponteiro do arquivo como NULL
+        }
+    }
+
+    printf("Números foram adicionados ao arquivo '%s'.\n", nomeArquivo);
+}
+
